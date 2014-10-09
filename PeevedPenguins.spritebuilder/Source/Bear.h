e917dc473a6835a7f8b37513d619d9b15d4e6e71
Mark Williams <markjwill@gmail.com>
2014-10-08 20:05:57 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Bear.h
@@ -0,0 +1,13 @@
+//
+//  Bear.h
+//  PeevedPenguins
+//
+//  Created by Mark Williams on 10/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Bear : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Bear.m
@@ -0,0 +1,13 @@
+//
+//  Bear.m
+//  PeevedPenguins
+//
+//  Created by Mark Williams on 10/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Bear.h"
+
+@implementation Bear
+
+@end
